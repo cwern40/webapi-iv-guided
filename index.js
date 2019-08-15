883663015eb78e4ee3b7234c71a6a78ff011a1c0
cwern40 <chris.wernli@hotmail.com>
2019-08-14 18:21:41 -0600

    guided project

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+require('dotenv').config();
+
 const server = require('./api/server.js');
 
-server.listen(4000, () => {
-  console.log('\n*** Server Running on http://localhost:4000 ***\n');
+//setting the port to the variable that Heroku assigns or the default of 4000
+const port = process.env.PORT || 4000;
+
+server.listen(port, () => {
+  console.log(`\n*** Server Running on ${port} ***\n`);
 });
